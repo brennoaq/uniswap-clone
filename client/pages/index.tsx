31eacf50b73551947ec768ab1bb9cb7f5852133f
brenno <brennoaraujoqueiroz@gmail.com>
2022-08-24 00:36:14 -0300

    Implements Header section

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header/Header"
 
 const style = {
-  wrapper: `h-screen max-h-screen h-min-screen w-screen -bg-[#2D242F] text-white select-none flex flex-col justify-between`
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`
 }
 
 export default function  Home(){
